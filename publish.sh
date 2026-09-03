f0782129bbfc5d5ecb1f0933fee0cb9f29114fa0
Benoit Petit <bpetit@hubblo.org>
2026-09-03 09:30:30 +0200

    New content

--- publish.sh
+++ publish.sh
@@ -3,8 +3,9 @@
 #set -e
 
 hugo
-echo ".post__content { width: 65%; }" >> docs/css/main.css
-echo ".post__content { width: 65%; }" >> docs/css/main-minimal.css
+#echo ".post__content { width: 65%; }" >> docs/css/main.css
+#echo ".post__content { width: 65%; }" >> docs/css/main-minimal.css
+echo "div .post { width: 65% }" >> docs/css/anatole.css
 git add *
 git commit -m "New content"
 git push -f
